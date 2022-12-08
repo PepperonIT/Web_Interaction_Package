5ff2f096455afe536c407417fb61d7bef8e7c57a
erik99s <erisre-8@student.ltu.se>
2022-12-08 14:33:38 +0100

    added wikipedia code

--- src/ask_pepperonit.py
+++ src/ask_pepperonit.py
@@ -1,8 +1,32 @@
 """ Testing package pypi packaging with a simple project
 """
 
-def say_hello(name=None):
-    if name is None:
-        return "Hello, World!"
-    else:
-        return f"Hello, {name}!"
+import wikipedia
+
+def ask_wikipedia_api(question, wiki_lang):
+    """
+    End half of ask_wikipedia, it calls the helper function get_info_wikipedia
+    question: the string input to wiki api
+    wiki_lang: the language for wikipedia to search in, e.g. "sv"
+    """
+    summary, image = get_info_wikipedia(question, wiki_lang)
+    return summary, image
+    
+    
+def get_info_wikipedia(term, wiki_lang):
+    """
+    term: A string as input into a wikipedia search
+    """
+    wikipedia.set_lang(wiki_lang)
+    summary = wikipedia.summary(term, sentences=2)
+    try:
+        page = wikipedia.page(term)
+        image = page.images[0] 
+        print("[INFO]: wiki image: " + image)# pylint: disable=superfluous-parens
+        return summary, image
+
+    except IndexError as error:
+        print(error)# pylint: disable=superfluous-parens
+
+    return (summary, 
+    "https://upload.wikimedia.org/wikipedia/commons/6/61/Wikipedia-logo-transparent.png")
